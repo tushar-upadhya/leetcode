087812b80fb681bef4d06d8aad7afdd84bf13665
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-01-31 07:36:03 +0530

    Time: 52 ms (60.95%), Space: 48.8 MB (11.20%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    return arr.reduce((result, value, index) => {
+        if(fn(value, index)){
+            result.push(value);
+        }
+        return result;
+    },[])
+};
