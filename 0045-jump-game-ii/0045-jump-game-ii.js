179c9d1458c3449cf418fc275eb63a68b7992b1d
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-03-20 09:00:03 +0530

    Time: 44 ms (97.84%), Space: 50.8 MB (77.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let newMax = 0;
+    let jump =  0;
+    let oldMax = 0;
+    
+    for(let i = 0; i < nums.length - 1; i++){
+        newMax = Math.max(newMax, i + nums[i]);
+        
+        if(i == oldMax){
+            jump++;
+            oldMax = newMax
+        }
+    }
+    return jump;
+};
