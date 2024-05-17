369c232ea7ec9bfa03870dd8fc568ac351020473
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-05-17 12:32:48 +0530

    Time: 53 ms (58.25%), Space: 49.7 MB (58.25%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+    let mid;
+    
+    while(low <= high){
+        mid = Math.floor((low + high) / 2);
+        if(nums[mid] >= target) high = mid - 1;
+        else low = mid + 1;
+    }
+    if(nums[low] !== target) return[-1, -1];
+    
+    const start = low;
+    low = 0;
+    high = nums.length - 1;
+    
+    while(low <= high){
+        mid = Math.floor((low + high) / 2);
+        if(nums[mid] <= target) low = mid + 1
+        else high = mid - 1;
+    }
+    return [start, high];
+};
