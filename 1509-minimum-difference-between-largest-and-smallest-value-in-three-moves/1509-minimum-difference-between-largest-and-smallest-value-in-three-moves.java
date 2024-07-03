b842c8fd0fd28d1f0e384404181f3907dc2ae2e5
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-07-03 08:42:46 +0530

    Time: 15 ms (96.06%), Space: 56.8 MB (83.99%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDifference(int[] nums) {
+
+        int n = nums.length;
+        
+        if(n <= 4) return 0;
+
+        Arrays.sort(nums);
+        
+        int a = nums[n - 4] - nums[0];
+        int b = nums[n - 3] - nums[1];
+        int c = nums[n - 2] - nums[2];
+        int d = nums[n - 1] - nums[3];
+
+        return Math.min(a, Math.min(b, Math.min(c, d)));        
+    }
+}
