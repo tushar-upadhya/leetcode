164cf3b1d5e08f3b4c92f2fe6f3bbc5241ad0a5a
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-07-06 09:35:53 +0530

    Time: 66 ms (6.10%), Space: 48.9 MB (20.73%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function(n, time) {
+    const chunks = Math.floor(time / (n - 1));
+
+    return chunks % 2 === 0 ? (time % (n - 1) + 1) : (n - time % (n - 1))
+};
