21218540551bead980f30e760ff0cff4140f3279
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2024-12-07 09:31:27 +0530

    Time: 537 ms (18.72%), Space: 45.6 MB (20.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,33 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-
-        if (n < 2) {
+        if(s.length() <= 1){
             return s;
-        }
+        }    
+        int maxi = 1;
+        String str = s.substring(0, 1);
 
-        String sub = s.substring(0, 1);
-        int curr = 1;
-
-        for (int i = 0; i < n; i++) {
-            
-            int start = i, end = i;
-            while (start >= 0 && end < n && s.charAt(start) == s.charAt(end)) {
-
-                if (end - start + 1 > curr) {
-                    sub = s.substring(start, end + 1);
-                    curr = end - start + 1;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i + maxi; j <= s.length(); j++){
+                if(j - i > maxi && helper(s.substring(i, j))){
+                    maxi = j - i;
+                    str = s.substring(i, j);
                 }
-                start--;
-                end++;
-            }
-
-            start = i;
-            end = i + 1;
-            
-            while (start >= 0 && end < n && s.charAt(start) == s.charAt(end)) {
-                if (end - start + 1 > curr) {
-                    sub = s.substring(start, end + 1);
-                    curr = end - start + 1;
-                }
-                start--;
-                end++;
             }
         }
-        return sub;
+        return str;
+    }
+
+    private boolean helper(String str){
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
