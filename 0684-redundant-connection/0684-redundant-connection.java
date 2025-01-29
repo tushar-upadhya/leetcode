9d43823fdd9d8cbbc515c0d0f5bc420e1d181e9d
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-01-29 09:44:19 +0530

    Time: 7 ms (15.25%), Space: 45 MB (18.17%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+
+        for(int edge[] : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(adj.containsKey(u) && adj.containsKey(v)){
+                boolean visited[] = new boolean[n + 1];
+
+                if(helper(adj, u, v, visited)){
+                    return edge;
+                }
+            }
+
+            adj.putIfAbsent(u, new ArrayList<>());
+            adj.putIfAbsent(v, new ArrayList<>());
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        return new int[0];
+    }
+    private boolean helper(Map<Integer, List<Integer>> adj, int u, int v, boolean[] visited) {
+        visited[u] = true;
+        if (u == v) {
+            return true;
+        }
+        for (int ngbr : adj.get(u)) {
+            if (visited[ngbr]) continue;
+            if (helper(adj, ngbr, v, visited)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
