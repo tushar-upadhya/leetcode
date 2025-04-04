c97651ae913aa221b39d853c7f1a13511ec94273
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-04-04 15:23:57 +0530

    Time: 15 ms (11.52%), Space: 45 MB (19.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set set = new HashSet<>();
+
+        for(int i = 0; i < 9; ++i){
+            for(int j = 0; j < 9; ++j){
+                char number = board[i][j];
+
+                if(number != '.')
+                if (!set.add(number + " in row " + i) ||
+                    !set.add(number + " in column " + j) ||
+                    !set.add(number + " in block " + i / 3 + "-" + j / 3))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
