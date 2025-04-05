9824b65b6ef7a9551c899e7a6525d08551a5fd8c
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-04-05 10:38:26 +0530

    Time: 52 ms (9.91%), Space: 57.5 MB (62.57%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        Map<Integer, Integer> firstMap = new HashMap<>();
+        Map<Integer, Integer> secondMap = new HashMap<>();
+        int n = nums.size();
+
+        for(int num : nums){
+            secondMap.put(num, secondMap.getOrDefault(num, 0) + 1);
+        }
+
+        for(int index = 0; index < n; index++){
+            int num = nums.get(index);
+            secondMap.put(num, secondMap.get(num) - 1);
+            firstMap.put(num, firstMap.getOrDefault(num, 0) + 1);
+
+            if(firstMap.get(num) * 2 > index + 1 && secondMap.get(num) * 2  > n - index - 1){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
