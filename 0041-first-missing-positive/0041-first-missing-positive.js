1c83d1c8380788b73f381c0d6e94c92abe5cdde7
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-04-12 10:53:19 +0530

    Time: 3 ms (79.01%), Space: 62 MB (83.27%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let i = 0;
+
+    while(i < nums.length){
+        let j = nums[i] - 1;
+
+        if(nums[i] < nums.length && nums[i] > 0 && nums[i] !== nums[j]){
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }else{
+            i++;
+        }
+    }
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== i + 1){
+            return i + 1;
+        }
+    }
+    return nums.length + 1;
+};
