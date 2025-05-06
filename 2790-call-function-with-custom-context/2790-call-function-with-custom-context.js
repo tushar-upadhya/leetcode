541e8e5188d4afdab8511033774bd86e70ca0e4c
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-05-06 10:28:29 +0530

    Time: 42 ms (69.27%), Space: 54 MB (23.74%) - LeetHub

---
+++ 2790-call-function-with-custom-context/2790-call-function-with-custom-context.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Object} context
+ * @param {Array} args
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+    const fn = this;
+    Object.prototype.func = fn;
+
+    return context.func(...args)
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
