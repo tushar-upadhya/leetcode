5335bee7cd1a33714968af90beda336f2b2a6b85
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-05-08 11:01:53 +0530

    Time: 387 ms (9.17%), Space: 105.3 MB (24.01%) - LeetHub

---
+++ 2858-join-two-arrays-by-id/2858-join-two-arrays-by-id.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    let map = {};
+    [...arr1, ...arr2].forEach(e => map[e.id] = {...map[e.id], ...e});
+
+    return Object.values(map)
+};
