2272f81113944cb16557d7dc92326aa63902a177
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-05-27 15:18:08 +0530

    Time: 0 ms (100%), Space: 57 MB (74.58%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.ts
@@ -0,0 +1,12 @@
+function differenceOfSums(n: number, m: number): number {
+    let result = 0;
+
+    for(let i = 0; i <= n; i++){
+        if(i % m === 0){
+            result -= i;
+        }else{
+            result += i;
+        }
+    }
+    return result;
+};
