3692ca9906d46f13b466aeba16ed350392527906
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-06-02 16:20:05 +0530

    Time: 3 ms (84.79%), Space: 45.7 MB (80.33%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,24 @@
-public class Solution {
+class Solution {
     public int candy(int[] ratings) {
-        if (ratings == null || ratings.length == 0) return 0;
-        
-        int total = 1;
-        int prev = 1;
-        int countDown = 0;
-        
-        for (int i = 1; i < ratings.length; i++) {
-            if (ratings[i] >= ratings[i-1]) {
-                if (countDown > 0) {
-                    total += countDown*(countDown+1)/2; 
-                    if (countDown >= prev) total += countDown - prev + 1;
-                    countDown = 0;
-                    prev = 1;
-                }
-                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
-                total += prev;
-            } else countDown++;
+        int left[] = new int[ratings.length];
+        int right[] = new int[ratings.length];
+
+        left[ratings.length - 1] = 1;
+        right[0] = 1;
+
+        for(int i = 1; i < ratings.length; i++){
+            right[i] = ratings[i] > ratings[i - 1] ? right[i - 1] + 1 : 1;
         }
-        if (countDown > 0) { 
-            total += countDown*(countDown+1)/2;
-            if (countDown >= prev) total += countDown - prev + 1;
+
+        for(int i = ratings.length - 2; i >= 0; i--){
+            left[i] = ratings[i] > ratings[i + 1] ? left[i + 1] + 1 : 1; 
         }
-        return total;
+
+        int count = 0;
+
+        for(int i = 0; i < ratings.length; i++){
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
     }
 }
