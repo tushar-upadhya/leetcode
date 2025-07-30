c738ca48ece1b4ee31ff82faf0c6c8d64b025d04
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-07-30 16:21:07 +0530

    Time: 4 ms (19.74%), Space: 40.5 MB (59.38%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,12 +1,10 @@
 class Solution {
-    int count = 0;
-    
-    public int totalNQueens(int n) {
-        char[][] board=new char[n][n];
-
+        int count = 0;
+        public int totalNQueens(int n) {
+        char board[][] = new char[n][n];
+        
         for(int i = 0; i < n; i++){
-
-            for(int j = 0; j < n; j++){
+            for(int j = 0;j < n; j++){
                 board[i][j]='.';
             }
         }
@@ -15,46 +13,43 @@ class Solution {
         return count;
     }
 
-    public boolean boardgenerater(char[][] board,int col, int n){
+    private boolean boardgenerater(char board[][],int col,int n){
         if(col == n){
             count++;
             return false;
 
         }
-        
+
         for(int i = 0; i < n; i++){
             
             if(issafe(board, i, col, n)){
                 board[i][col] = 'Q';
                
-               if(boardgenerater(board, col + 1, n)){
+               if( boardgenerater(board, col + 1, n)){
                    return true;
                }else{
-                   board[i][col]='.';
+                   board[i][col] = '.';
                }
             }
         }
         return false;
     }
 
-    public boolean issafe(char[][] board, int row, int col, int n){
+    private boolean issafe(char board[][], int row, int col, int n){
         int x = row;
         int y = col;
 
         for(int i = 0; i < n; i++){
-
-            if(board[i][col] == 'Q'){
+            if(board[i][col ]== 'Q'){
                 return false;
             }
 
             if(board[row][i] == 'Q'){
                 return false;
             }
-
         }
 
         while(x < n && y >= 0){
-            
             if(board[x][y] == 'Q'){
                 return false;
             }
@@ -63,8 +58,7 @@ class Solution {
         }
 
         while(row >= 0 && col >= 0){
-            
-            if(board[row][col] =='Q'){
+            if(board[row][col] == 'Q'){
                 return false;
             }
             row--;
