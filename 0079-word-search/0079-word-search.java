8e5386999e46e35ba1a119e148728afa69d520c2
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2025-07-31 14:46:03 +0530

    Time: 147 ms (47.46%), Space: 41.6 MB (77.43%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,33 +1,43 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++)
-                if((board[i][j] == word.charAt(0)) && helper(board, i, j, 0, word))
-                    return true;
-            
-            // return false;
+        int m = board.length;
+        int n = board[0].length;
+
+        boolean visited[][] = new boolean[m][n];
+        boolean result = false;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0)){
+                    result = helper(board, word, visited, i, j, 0);
+
+                    if(result) return true;
+                }
+            }
         }
         return false;
     }
-    
-    private boolean helper(char board[][], int i, int j, int count, String word){
-        if(count == word.length()) return true;
-        
-        if(i == -1 || i == board.length || j == -1 || j == board[0].length || board[i][j] != word.charAt(count))
+
+    private boolean helper(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if(index == word.length()){
+            return true;
+        }
+
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)){
             return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '*';
-        
-        boolean found = helper(board, i + 1, j, count + 1, word) || 
-            helper(board, i - 1, j, count + 1, word) ||
-            helper(board, i, j + 1                  
-                   , count + 1, word) ||
-            helper(board, i, j -1                 
-                   , count + 1, word);
-            
-            board[i][j] = temp;
-        
-        return found;
+        }
+
+        visited[i][j] = true;
+
+        if(helper(board, word, visited, i + 1, j, index + 1) ||
+           helper(board, word, visited, i - 1, j, index + 1) || 
+           helper(board, word, visited, i, j + 1, index + 1) ||
+           helper(board, word, visited, i, j - 1, index + 1)){
+            return true;
+           }
+
+           visited[i][j] = false;
+
+           return false;
     }
 }
